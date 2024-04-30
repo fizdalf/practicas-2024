76bf34c4e57f7be77d9f026270074271a66d54bb
Neyder17 <103927895+Neyder17@users.noreply.github.com>
2024-04-30 10:50:10 +0100

    Cambios

--- Poyecto-User-Stories/userStories.php
+++ Poyecto-User-Stories/userStories.php
@@ -1,41 +1,57 @@
 <?php
+interface BookRepositoryInterface{
+    public function getBooks(): array;
+}
+interface LoanRepositoryInterface{
+    public function getLoans(): array;
+}
 
-class userStories
-{
-    public static function userRegister()
+class UserStories{
+    private $bookrepository;
+    private $loanRepository;
+
+    public function __construct(BookRepositoryInterface $bookRepository, LoanRepositoryInterface $loanRepository)
+    {
+        $this->bookrepository = $bookRepository;
+        $this->loanRepository = $loanRepository;
+    }
+
+    public function userRegister()
     {
         return "registrao";
     }
 
-    public static function userLogin($email, $password)
+    public function userLogin($email, $password)
     {
         $emailCorrecto = "usuario@gmail.com";
         $passwordCorrecto = "1234";
 
-        if ($email === $emailCorrecto && $password === $passwordCorrecto){
+        if ($email === $emailCorrecto && $password === $passwordCorrecto) {
             return "Credenciales Correctas";
         } else {
             return "Credenciales Incorrectas";
         }
     }
 
-    public static function listAvailableBooks($books, $loans){
-        $availableBooks = array();
-        foreach ($books as $book){
-            if(!self::isBookOnLoan($book["id"], $loans)) {
-                $availableBooks[] = $book;
+    public function listAvaibleBooks()
+    {
+        $books = $this->bookrepository->getBooks();
+        $loans = $this->loanRepository->getLoans();
+        $avaibleBooks = array();
+        foreach ($books as $book) {
+            if (!$this->isBookOnLean($book["id"], $loans)){
+                $avaibleBooks[] = $book;
             }
         }
-        return $availableBooks;
+        return $avaibleBooks;
     }
 
-    public static function isBookOnLoan($bookId, $loans)
-    {
-        foreach ($loans as $loan) {
-            if ($loan['book_id'] == $bookId && !$loan['returned']) {
+    public function isBookLoan($bookId, $loans){
+        foreach ($loans as $loan){
+            if ($loan["book_id"] == $bookId && !~$loan["returned"]){
                 return true;
             }
         }
-        return false;
+        return true;
     }
 }
