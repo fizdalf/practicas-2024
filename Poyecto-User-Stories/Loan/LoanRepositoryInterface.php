58e2abe1a07840d2ca91283df05be94d38b559c4
Neyder17 <103927895+Neyder17@users.noreply.github.com>
2024-05-08 10:20:19 +0100

    Changes by aguil

--- Poyecto-User-Stories/Loan/LoanRepositoryInterface.php
+++ Poyecto-User-Stories/Loan/LoanRepositoryInterface.php
@@ -1,5 +1,16 @@
 <?php
 
 class LoanBook{
-
+    public function listAvailableBooks()
+    {
+        $books = $this->bookRepository->getBooks();
+        $loans = $this->loanRepository->getLoans();
+        $availableBooks = array();
+        foreach ($books as $book) {
+            if (!$this->isBookOnLoan($book["id"], $loans)){
+                $availableBooks[] = $book;
+            }
+        }
+        return $availableBooks;
+    }
 }
