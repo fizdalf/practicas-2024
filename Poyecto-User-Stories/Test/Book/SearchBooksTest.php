45fdf52349a92fd47b22a8f48c06932a1df27422
Neyder17 <103927895+Neyder17@users.noreply.github.com>
2024-05-09 15:18:30 +0100

    Progreso x77

--- Poyecto-User-Stories/Test/Book/SearchBooksTest.php
+++ Poyecto-User-Stories/Test/Book/SearchBooksTest.php
@@ -51,11 +51,42 @@ class SearchBooksTest extends TestCase
         $this->assertEquals($expectedResult, $searchResult);
     }
 
+    public function testSearchByAuthor()
+    {
+        // Preparar datos de prueba (libros)
+        $books = [
+                ['id' => 1, 'title' => 'Cien años de soledad', 'author' => 'Gabriel García Márquez', 'publisher' => 'Editorial Sudamericana'],
+                ['id' => 2, 'title' => 'Don Quijote de la Mancha', 'author' => 'Miguel de Cervantes', 'publisher' => 'Editorial Planeta'],
+                ['id' => 3, 'title' => 'El código Da Vinci', 'author' => 'Dan Brown', 'publisher' => 'Editorial Norma']
+        ];
+
+        $bookRepositoryMock = $this->createMock(BookRepositoryInterface::class);
+        $bookRepositoryMock->method('getBooks')->willReturn($books);
+
+        $this->userStories = new UserStories($bookRepositoryMock, $loanRepositoryMock);
+
+        $searchResult = $this->userStories->searchBooks("Gabriel García Márquez");
+
+        $expectedResult = [['id' => 1, 'title' => 'Cien años de soledad', 'author' => 'Gabriel García Márquez', 'publisher' => 'Editorial Sudamericana']];
+        $this->assertEquals($expectedResult, $searchResult);
+    }
+
     public function testSearchByPublisher()
     {
         $books = [
                 ['id' => 1, 'title' => 'Cien años de soledad', 'author' => 'Gabriel García Márquez', 'publisher' => 'Editorial Sudamericana'],
-                ['id' => 2, 'title' => 'Don Quijote de la Mancha', 'author' => 'Miguel de Cervantes', 'publisher' => 'Editorial Plan
+                ['id' => 2, 'title' => 'Don Quijote de la Mancha', 'author' => 'Miguel de Cervantes', 'publisher' => 'Editorial Planeta'],
+                ['id' => 3, 'title' => 'El código Da Vinci', 'author' => 'Dan Brown', 'publisher' => 'Editorial Norma']
+        ];
 
+        $bookRepositoryMock = $this->createMock(BookRepositoryInterface::class);
+        $bookRepositoryMock->method('getBooks')->willReturn($books);
 
+        $this->userStories = new UserStories($bookRepositoryMock, $loanRepositoryMock);
+
+        $searchResult = $this->userStories->searchBooks("Editorial Sudamericana");
+
+        $expectedResult = [['id' => 1, 'title' => 'Cien años de soledad', 'author' => 'Gabriel García Márquez', 'publisher' => 'Editorial Sudamericana']];
+        $this->assertEquals($expectedResult, $searchResult);
+    }
 }
