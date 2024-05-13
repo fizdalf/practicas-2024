53273566696c0489c3864942c877cc89240c962b
Neyder17 <103927895+Neyder17@users.noreply.github.com>
2024-05-13 13:33:58 +0100

    BookLoan 1/2

--- Poyecto-User-Stories/Loan/BookLoan.php
+++ Poyecto-User-Stories/Loan/BookLoan.php
@@ -4,5 +4,14 @@ namespace Practicas2024\BookLoanSystem\Loan;
 
 class BookLoan
 {
+    private string $status;
+    public function __construct($a, $b, $c, $d,string $status)
+    {
+        $this->status = $status;
+    }
 
+    public function status(): string
+    {
+        return  $this->status;
+    }
 }
---
+++ Poyecto-User-Stories/Test/Loan/BookLoanTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Practicas2024\BookLoanSystem\Loan;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+require __DIR__.'/../../Loan/BookLoan.php';
+
+class BookLoanTest extends TestCase
+{
+    function testShouldReturnStatusInBookLoan(){
+        $bookLoan = new BookLoan(
+                1,
+                1,
+                1,
+                new DateTimeImmutable('2021-01-01 10:35:00'),
+                "PENDING"
+        );
+
+        $this->assertEquals('PENDING', $bookLoan->status());
+
+    }
+}
