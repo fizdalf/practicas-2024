c383bd6ddf62421d3aaf0b173011526f66b508d7
Neyder17 <103927895+Neyder17@users.noreply.github.com>
2024-05-13 12:56:33 +0100

    Cambios avance

--- Poyecto-User-Stories/Loan/BookReturnProcessor.php
+++ Poyecto-User-Stories/Loan/BookReturnProcessor.php
@@ -5,9 +5,15 @@ namespace  Practicas2024\BookLoanSystem\Loan;
 class BookReturnProcessor
 {
 
+    private PendingToReturnBookLoanFinder $finder;
+
+    public function __construct(PendingToReturnBookLoanFinder $finder)
+    {
+        $this->finder = $finder;
+    }
 
     public function __invoke(int $bookId)
     {
-        
+        $this->finder->__invoke($bookId);
     }
 }
