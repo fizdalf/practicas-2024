8679aa3c9854ea21c34ba58afbf0a84e51ae18f7
Neyder17 <103927895+Neyder17@users.noreply.github.com>
2024-05-17 12:39:13 +0100

    Fallo en el primer test + Correccion

--- Poyecto-User-Stories/Test/Loan/AddBookTest.php
+++ Poyecto-User-Stories/Test/Loan/AddBookTest.php
@@ -14,4 +14,22 @@ class AddBookTest extends TestCase
         $loanRepositoryMock = $this->createMock(LoanRepositoryInterface::class);
         $this->userStories = new UserStories($this->bookRepositoryMock, $loanRepositoryMock);
     }
+
+    public function testShouldAddBookSuccessfully()
+    {
+        $newBook = [
+                'id' => 4,
+                'title' => '1984',
+                'author' => 'George Orwell',
+                'publisher' => 'Secker & Warburg'
+        ];
+
+        $this->bookRepositoryMock->expects($this->once())
+                ->method('addBook')
+                ->with($newBook)
+                ->willReturn(true);
+
+        $result = $this->userStories->addBook($newBook);
+        $this->assertTrue($result);
+    }
 }
